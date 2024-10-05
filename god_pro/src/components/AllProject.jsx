39e53bed281d4b7fc95127820ff208c243c8cc9e
jogpfls <joerin4177@gmail.com>
2024-10-05 21:42:16 +0900

    fix: UI 수정

--- god_pro/src/components/AllProject.jsx
+++ god_pro/src/components/AllProject.jsx
@@ -5,19 +5,19 @@ import { useNavigate } from 'react-router-dom';
 const AllProject = () => {
   const navigate = useNavigate();
   return (
-    <div className='allWrapper'>
-      <div className='wrapper'>
-        <div className='topWrapper'>
+    <div className='allProject_wrapper_allWrapper'>
+      <div className='allProject_wrapper'>
+        <div className='allProject_wrapper_topWrapper'>
           <div className='employWrapper'>
             <p className='employText'>모집 중</p>
           </div>
-          <p className='text'>Terraform으로 Azure Clolud 리소스 배포</p>
-          <p className='count'>3 / 8명</p>
-          <p className='contents'>이건 무슨무슨 프로젝트 (한줄소개)</p>
+          <p className='allProject_wrapper_text'>Terraform으로 Azure Clolud 리소스 배포</p>
+          <p className='allProject_wrapper_count'>3 / 8명</p>
+          <p className='allProject_wrapper_contents'>이건 무슨무슨 프로젝트 (한줄소개)</p>
         </div>
-        <div className='bottomWrapper'>
+        <div className='allProject_wrapper_bottomWrapper'>
             <button 
-            className='btn'
+            className='allProject_wrapper_btn'
             onClick={()=>navigate('/detailpage/lookup')}
             >자세히보기</button>
         </div>
