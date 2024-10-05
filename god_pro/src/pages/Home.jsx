7c2a04f424fc69eb9c1eed6c21586788e2430639
jogpfls <joerin4177@gmail.com>
2024-10-05 21:50:42 +0900

    fix: UI 수정

--- god_pro/src/pages/Home.jsx
+++ god_pro/src/pages/Home.jsx
@@ -3,15 +3,23 @@ import styled from 'styled-components';
 import AllProject from '../components/AllProject';
 import bannerImg from '../images/image.svg'
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
+import Header from '../components/Header';
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
+    <>
+    <Header />
     <div className='home_wrapper'>
       <div className='home_banner'>
         <div className='home_innerWrapper'>
           <div>
             <p className='home_titleText'>함께하는 프로젝트</p>
             <p className='home_titleText'>함께 성장하는 개발자들의 모임</p>
+            <button 
+            onClick={()=>navigate('/detailpage/Register')}
+            className="home_button">프로젝트 생성하기</button>
           </div>
         <img className='home_bannerImage' src={bannerImg} alt="이미지" />
         </div>
@@ -25,6 +33,7 @@ const Home = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
