c7136eb36d9abcd3448fb2ed7aa603f45ea02c3a
jogpfls <joerin4177@gmail.com>
2024-10-06 00:10:48 +0900

    feat: 새로운 페이지 라우터 추가

--- god_pro/src/App.jsx
+++ god_pro/src/App.jsx
@@ -9,13 +9,15 @@ import Modify2 from "./pages/MyPage/Modify2";
 import Register2 from "./pages/MyPage/Register2";
 import Profile from "./pages/MyPage/Profile";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import SubmitCheck from './pages/SubmitCheck';
+import SubmitPerson from './pages/SubmitPerson';
 
 function App() {
   const navigate = useNavigate();
 
   return (
     <>
-       <Routes>
+        <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/detailpage/lookup" element={<Lookup />} />
@@ -25,6 +27,8 @@ function App() {
             <Route path="/mypage/modify2" element={<Modify2 />} />
             <Route path="/mypage/profile" element={<Profile />} />
             <Route path="/mypage/register2" element={<Register2 />} />
+            <Route path="/mypage/submitCheck" element={<SubmitCheck />}/>
+            <Route path="/mypage/submit_check/person" element={<SubmitPerson/>}/>
         </Routes>
     </>
   )
