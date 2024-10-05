560bd781305fdda6df0b92cd3cc3cda63df68401
jogpfls <joerin4177@gmail.com>
2024-10-05 22:11:03 +0900

    feat: Button2 UI 구현

--- god_pro/src/pages/Mypage/Button2.jsx
+++ god_pro/src/pages/Mypage/Button2.jsx
@@ -1,9 +1,22 @@
+import './Button2.css';
+import Header from '../../components/Header';
+import { useNavigate } from 'react-router-dom';
+
 const Button2 = () => {
-	return (
+	const navigate = useNavigate();
+  return (
 		<div>
-            버튼 페이지입니다.
+			<Header/>
+			<div className='button2_allWrapper'>
+					<button 
+					onClick={()=>navigate('/mypage/profile')}
+					className='button2_buttonWrapper'>프로필</button>
+					<button 
+					onClick={()=>navigate('/mypage/register2')}
+					className='button2_buttonWrapper'>등록 프로젝트</button>
+			</div>
 		</div>
-	);
+  );
 };
 
 export default Button2;
