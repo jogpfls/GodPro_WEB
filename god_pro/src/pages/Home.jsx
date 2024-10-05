8f11f247c5e1c8c3b65f7f8f92a1ee1ff76e912b
jogpfls <joerin4177@gmail.com>
2024-10-05 21:43:34 +0900

    feat: 메인페이지 UI 구현

--- god_pro/src/pages/Home.jsx
+++ god_pro/src/pages/Home.jsx
@@ -1,11 +1,78 @@
-import Header from "../components/Header";
+import React from 'react';
+import styled from 'styled-components';
+import AllProject from '../components/AllProject';
+import bannerImg from '../images/image.svg'
+import './Home.css';
 
 const Home = () => {
-	return (
-		<div>
-			<Header />
-		</div>
-	);
+  return (
+    <div className='home_wrapper'>
+      <div className='home_banner'>
+        <div className='home_innerWrapper'>
+          <div>
+            <p className='home_titleText'>함께하는 프로젝트</p>
+            <p className='home_titleText'>함께 성장하는 개발자들의 모임</p>
+          </div>
+        <img className='home_bannerImage' src={bannerImg} alt="이미지" />
+        </div>
+      </div>
+      <div>
+      <p className='home_text'>기본 정렬 순</p>
+        <div className='home_projectWrapper'>
+          <AllProject />
+          <AllProject />
+          <AllProject />
+        </div>
+      </div>
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Banner = styled.div`
+  background-color: #FAECE2;
+  width: 100%;
+  height: 350px;
+  margin-bottom: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InnerWrapper = styled.div`
+  display: flex;
+  width: 950px;
+  justify-content: space-between;
+`;
+
+const TitleText = styled.p`
+  font-size: 25px;
+  font-weight: 600;
+`;
+
+const BannerImage = styled.img`
+  width: 400px;
+`;
+
+const Text = styled.p`
+  width: 100%;
+  text-align: end;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 10px;
+`;
+
+const ProjectWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
 
 export default Home;
