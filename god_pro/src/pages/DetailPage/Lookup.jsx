dcbd9fbd3a645d7ab8a2beb29c0308caf8bbe8f5
YU-JIN-JUNG <yuuuujiiiin@naver.com>
2024-10-05 21:37:51 +0900

    feat: 디테일 페이지 버튼 컴포넌트 생성

--- god_pro/src/pages/DetailPage/Lookup.jsx
+++ god_pro/src/pages/DetailPage/Lookup.jsx
@@ -1,9 +1,14 @@
+import Button from '../../components/Button';
+import Header from "../../components/Header";
+
 const Lookup = () => {
 	return (
 		<div>
-            조회 페이지입니다.
+			<Header />
+            <Button text={'등록하기'} />
+			<Button text={'수정하기'} type={'SMALLER'} />
 		</div>
 	);
 };
 
-export default Lookup;
+export default Lookup; 
