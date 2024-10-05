0b011cd7f46a4bbbdd6ead66da45803d46d030d3
jogpfls <joerin4177@gmail.com>
2024-10-06 02:57:29 +0900

    hotfix: 긴급 수정

--- god_pro/src/App.jsx
+++ god_pro/src/App.jsx
@@ -4,16 +4,15 @@ import Login from './pages/Login.jsx';
 import Lookup from './pages/DetailPage/Lookup.jsx';
 import Modify from './pages/DetailPage/Modify.jsx';
 import Register from './pages/DetailPage/Register.jsx';
-import Button2 from './pages/MyPage/Button2.jsx';
-import Modify2 from './pages/MyPage/Modify2.jsx';
-import Register2 from './pages/MyPage/Register2.jsx';
-import Profile from './pages/MyPage/Profile.jsx';
-import { Routes, Route, useNavigate } from "react-router-dom";
+import Button2 from './pages/Mypage/Button2.jsx';
+import Modify2 from './pages/Mypage/Modify2.jsx';
+import Register2 from './pages/Mypage/Register2.jsx';
+import Profile from './pages/Mypage/Profile.jsx';
+import { Routes, Route } from "react-router-dom";
 import SubmitCheck from './pages/SubmitCheck.jsx';
 import SubmitPerson from './pages/SubmitPerson.jsx';
 
 function App() {
-  const navigate = useNavigate();
 
   return (
     <>
