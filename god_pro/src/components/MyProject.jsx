12f6e70c0143de8f09bf10a8740a55fd81760582
jogpfls <joerin4177@gmail.com>
2024-10-06 00:08:36 +0900

    feat: 내가 등록한 프로젝트에 지원자 버튼 구현

--- god_pro/src/components/MyProject.jsx
+++ god_pro/src/components/MyProject.jsx
@@ -6,7 +6,12 @@ const MyProject = () => {
   const navigate = useNavigate();
   return (
     <div className='myProject_allWrapper'>
+      <div className='myProject_leftWrapper'>
       <p className='myProject_title'>00 프로젝트 모집</p>
+      <button 
+      onClick={()=>navigate(`/mypage/submitCheck`)}
+      className='myProject_submitCheck'>지원자 확인</button>
+      </div>
       <div className='myProject_buttonWrapper'>
         <button className='myProject_button'>삭제하기</button>
         <button 
